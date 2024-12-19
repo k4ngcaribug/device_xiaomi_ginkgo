24960a52d5d5a4578bae706178fd36949326ba7b
k4ngcaribug <168145100+k4ngcaribug@users.noreply.github.com>
2024-12-19 08:24:10 +0000

    ginkgo: Track some repos from ours git
    Signed-off-by: k4ngcaribug <168145100+k4ngcaribug@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,7 @@
 git clone --depth=1 https://github.com/ArrowOS-Devices/android_vendor_miuicamera -b arrow-12.0-a3 vendor/miuicamera
+rm -rf frameworks/base
+rm -rf frameworks/av
+rm -rf vendor/derp
+git clone --depth=1 https://github.com/k4ngcaribug/derp_frameworks_base frameworks/base
+git clone --depth=1 https://github.com/k4ngcaribug/derp_frameworks_av frameworks/av
+git clone --depth=1 https://github.com/k4ngcaribug/vendor_derp vendor/derp
