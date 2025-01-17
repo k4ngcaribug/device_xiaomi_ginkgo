31d12276d25e0559d3cbf4fa7199014873820833
k4ngcaribug <168145100+k4ngcaribug@users.noreply.github.com>
2025-01-17 12:11:18 +0000

    ginkgo: Go Arrow
    Signed-off-by: k4ngcaribug <168145100+k4ngcaribug@users.noreply.github.com>

--- arrow_ginkgo.mk
+++ arrow_ginkgo.mk
@@ -23,12 +23,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from ginkgo device
 $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 
-# Inherit some common DerpfestOS stuff
+# Inherit some common ArrowOS stuff
 TARGET_BOOT_ANIMATION_RES := 1080
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+DEVICE_MAINTAINER := neophyte
+DEVICE_MAINTAINER_LINK := https://t.me/k4ngcaribug
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Device identifier
-PRODUCT_NAME := derp_ginkgo
+PRODUCT_NAME := arrow_ginkgo
 PRODUCT_DEVICE := ginkgo
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 8/8T
--- vendorsetup.sh
+++
@@ -1,8 +0,0 @@
-rm -rf frameworks/base
-rm -rf frameworks/av
-rm -rf vendor/derp
-rm -rf packages/apps/Settings
-git clone --depth=1 https://github.com/k4ngcaribug/derp_frameworks_base frameworks/base
-git clone --depth=1 https://github.com/k4ngcaribug/derp_frameworks_av frameworks/av
-git clone --depth=1 https://github.com/k4ngcaribug/vendor_derp vendor/derp
-git clone --depth=1 https://github.com/k4ngcaribug/derp_packages_apps_Settings packages/apps/Settings
