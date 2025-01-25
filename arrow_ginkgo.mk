c8350b96458d8f824e06bf213563d526474667fb
k4ngcaribug <168145100+k4ngcaribug@users.noreply.github.com>
2025-01-25 06:31:20 +0000

    ginkgo: Arrow-Ify
    Signed-off-by: k4ngcaribug <168145100+k4ngcaribug@users.noreply.github.com>

--- arrow_ginkgo.mk
+++ arrow_ginkgo.mk
@@ -23,13 +23,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from ginkgo device
 $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 
-# Inherit some common DerpfestOS stuff
+# Inherit some common ArrowOS stuff
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_NOT_USES_BLUR := true
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Device identifier
-PRODUCT_NAME := derp_ginkgo
+PRODUCT_NAME := arrow_ginkgo
 PRODUCT_DEVICE := ginkgo
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 8/8T
