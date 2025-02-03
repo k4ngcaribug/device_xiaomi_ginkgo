a97b6c01c4343dcf7b172d36dc1667878e7a76b7
k4ngcaribug <168145100+k4ngcaribug@users.noreply.github.com>
2025-02-03 13:00:05 +0000

    ginkgo: switch to aosp-clang

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+git clone --depth=1 https://github.com/ArrowOS-Devices/android_vendor_xiaomi_ginkgo -b arrow-11.0 vendor/xiaomi/ginkgo
+git clone --depth=1 https://github.com/ArrowOS-Devices/android_vendor_miuicamera -b arrow-11.0-a3 vendor/miuicamera
+git clone --depth=1 https://github.com/k4ngcaribug/android_kernel_xiaomi_ginkgo -b 11 kernel/xiaomi/ginkgo
+git clone --depth=1 https://gitea.com/k4ngcaribug/aosp-clang prebuilts/clang/host/linux-x86/clang-aosp
