0f7724770536581f457ce16b481a284cfdf8a574
k4ngcaribug <168145100+k4ngcaribug@users.noreply.github.com>
2025-02-15 07:31:17 +0000

    Revert "ginkgo: Move to common Xiaomi light AIDL"
    This reverts commit ac9af0639a360888e88daf2888cd038421101ab0.

--- device.mk
+++ device.mk
@@ -234,7 +234,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.xiaomi
+    android.hardware.light@2.0-service.ginkgo
 
 # Lineage Health
 PRODUCT_PACKAGES += \
@@ -400,7 +400,6 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/xiaomi
 
 # Telephony
 PRODUCT_PACKAGES += \
