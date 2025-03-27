e64195f60cc26394c7ac4941a8536090fdc7dbd2
k4ngcaribug <neophyteplayground@gmail.com>
2025-03-27 16:00:41 +0700

    ginkgo: Import vendorsetup

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+git clone --depth=1 https://github.com/k4ngcaribug/vendor_xiaomi_ginkgo -b 10 vendor/xiaomi/ginkgo
+git clone --depth=1 https://github.com/k4ngcaribug/kernel_xiaomi_ginkgo -b 10 kernel/xiaomi/ginkgo
+git clone --depth=1 https://github.com/k4ngcaribug/android_vendor_miuicamera -b 10 vendor/miuicamera
+git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang -b main prebuilts/clang/host/linux-x86/clang-azure
