a89fea2e35a7cafb3eb157f6dd1c5f617496c005
neophyte <168145100+k4ngcaribug@users.noreply.github.com>
2025-04-15 20:26:10 +0700

    ginkgo: Switch to azure clang

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -137,12 +137,10 @@ TARGET_KERNEL_CONFIG := vendor/ginkgo_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
 TARGET_KERNEL_ADDITIONAL_FLAGS := AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := azure
 
 # Keystore
 TARGET_PROVIDES_KEYMASTER := true
-
-# LMKD
-TARGET_LMKD_STATS_LOG := true
 
 # Media
 TARGET_USES_MEDIA_EXTENSIONS := true
