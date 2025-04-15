55b25966dcebec0bf1ed8e1506b9b950c57a8ce9
neophyte <168145100+k4ngcaribug@users.noreply.github.com>
2025-04-15 23:51:39 +0700

    ginkgo: shorten wait time to optimize shutdown time
    Mount service will check if there exist processes occupy files. Launch
    many 3rd apps and set in background will cause many processes occupy
    files, which will cause mountservice spend 10-20s to (sometimes
    also unmount fail with 20s time out). If there is no process occupy
    files, mountservice only need about 500ms. So shorten wait time from
    20s to 500ms, which will save 10-20s for shutdown time, total shutdown
    time is in 3s.

--- device.mk
+++ device.mk
@@ -84,6 +84,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0 \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
+# Boot
+PRODUCT_PROPERTY_OVERRIDES += \
+     sys.vendor.shutdown.waittime=500
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
