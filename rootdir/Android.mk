1d53f16a19c6d0ce88fab6c039083ab12b02f225
neophyte <neophyteplayground@gmail.com>
2025-07-13 07:09:35 +0000

    ginkgo: rootdir: Reconfigure ZRAM

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -115,6 +115,13 @@ LOCAL_SRC_FILES    := etc/fstab.qcom
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.zram
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/fstab.zram
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fastcharge.sh
